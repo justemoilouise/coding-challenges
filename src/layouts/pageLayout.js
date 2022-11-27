e274cd0b5f70f2ca14e6247cdaa6f97e0eb70b7f
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-11-27 16:47:50 +1100

    toggle, animation, deps

--- src/layouts/pageLayout.js
+++ src/layouts/pageLayout.js
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { Header } from '../components/header/header';
 import { Content } from '../components/content/content';
 import { Footer } from '../components/footer/footer';
+import { DrawerProvider } from '../context/drawer-context';
 
 export const PageLayout = ({ children }) => (
-  <>
+  <DrawerProvider>
     <Header />
     <Content>{children}</Content>
     <Footer />
-  </>
+  </DrawerProvider>
 );
