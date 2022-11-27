f9fb066d30c95a8c1d9000612d492a0f544bf127
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-11-27 17:20:20 +1100

    drawer

--- src/layouts/pageLayout.js
+++ src/layouts/pageLayout.js
@@ -3,10 +3,12 @@ import { Header } from '../components/header/header';
 import { Content } from '../components/content/content';
 import { Footer } from '../components/footer/footer';
 import { DrawerProvider } from '../context/drawer-context';
+import { Drawer } from '../components/drawer/drawer';
 
 export const PageLayout = ({ children }) => (
   <DrawerProvider>
     <Header />
+    <Drawer />
     <Content>{children}</Content>
     <Footer />
   </DrawerProvider>
