1680c18e6d745e331ba3938062e076e18f2327e0
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-11-28 15:54:24 +1100

    page and layout

---
+++ src/layouts/codeAcademy-pageLayout.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { PageLayout } from './pageLayout';
+
+export const CodeAcademyPageLayout = ({ children }) => (
+  <PageLayout>
+    {children}
+  </PageLayout>
+);
---
+++ src/pages/code-academy.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { CodeAcademyPageLayout } from '../layouts/codeAcademy-pageLayout';
+
+const Page = () => {
+  return (
+    <CodeAcademyPageLayout></CodeAcademyPageLayout>
+  );
+};
+
+export default Page;
+
+export const Head = () => <title>Coding Challenges - Code Academy</title>;
