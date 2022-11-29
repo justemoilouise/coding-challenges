d7b6a39c9b183750cc8cb90e470491752b26b6b5
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-11-29 15:33:42 +1100

    card + challenge home grid + fixes

--- src/pages/code-academy/challenge-1.js
+++ src/pages/code-academy/challenge-1.js
@@ -12,10 +12,11 @@ const breadcumbItems = [
   },
 ];
 
-const details = {
+export const details = {
   title: 'Challenge 1 - Calculate the Mean and Mode',
   description: 'Given a list of numbers, return the list\'s mean and mode. If there are multiple modes, return the mode that occurs first.',
   link: 'https://www.codecademy.com/code-challenges/code-challenge-calculate-the-mean-and-mode-javascript',
+  tags: [ 'Algorithmic Complexity', 'Intermediate' ],
 };
 
 const Page = () => {
--- src/pages/code-academy/index.js
+++ src/pages/code-academy/index.js
@@ -1,9 +1,28 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
+import { Card } from '../../components/card/card';
+import { Grid } from '../../components/grid/grid';
+import { Pill } from '../../components/pill/pill';
 import { CodeAcademyPageLayout } from '../../layouts/codeAcademy-pageLayout';
+import { details as Challenge1Details } from './challenge-1';
 
 const Page = () => {
   return (
-    <CodeAcademyPageLayout></CodeAcademyPageLayout>
+    <CodeAcademyPageLayout>
+      <Grid>
+        <Link to='/code-academy/challenge-1'>
+          <Card>
+            <h4>Challenge 1</h4>
+            <p>Calculate the Mean and Mode</p>
+            <div>
+              {Challenge1Details.tags.map((tag, index) => (
+                <Pill key={index} variant='primary'>{tag}</Pill>
+              ))}
+            </div>
+          </Card>
+        </Link>
+      </Grid>
+    </CodeAcademyPageLayout>
   );
 };
 
