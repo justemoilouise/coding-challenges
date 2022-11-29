f274463a0bf03ba0e8596a6a985b0aba763abb7c
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-11-29 15:13:05 +1100

    include challenge link

--- src/layouts/codeAcademy-pageLayout.js
+++ src/layouts/codeAcademy-pageLayout.js
@@ -1,12 +1,26 @@
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { Breadcrumbs } from '../components/breadcrumbs/breadcrumbs';
 import { PageLayout } from './pageLayout';
 
-export const CodeAcademyPageLayout = ({ breadcumbItems, title, description, children }) => (
+export const CodeAcademyPageLayout = ({ breadcumbItems, title, description, link, children }) => (
   <PageLayout>
     <Breadcrumbs items={breadcumbItems} />
     {title && <h3>{title}</h3>}
-    {description && <p>{description}</p>}
+    {description && (
+      <p>
+        {description}
+        {link && (
+          <>
+            &nbsp;
+            <a href={link}>
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='2xs' />
+            </a>
+          </>
+        )}
+      </p>
+    )}
     {children}
   </PageLayout>
 );
