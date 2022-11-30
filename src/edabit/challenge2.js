2e2aabf170a9c2e75daeebd796b9ec3c7c0635c2
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-11-30 15:00:50 +1100

    challenge 2

---
+++ src/edabit/challenge2.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import * as styles from './challenge2.module.css';
+import { Pill } from '../components/pill/pill';
+
+export const Challenge2 = () => {
+  const inputRef = React.useRef();
+
+  const [seq, setSeq] = React.useState();
+
+  const handleClick = () => {
+    if (!inputRef?.current) {
+      return null;
+    }
+
+    setSeq(undefined);
+
+    const input = parseInt(inputRef.current.value);
+    if (input && input > 0) {
+      setSeq(getFibonacci([0], input));
+    }
+  };
+
+  const getFibonacci = (arr, len) => {
+    if (arr.length === len ) {
+      return arr;
+    }
+
+    if (arr.length < 2) {
+      return getFibonacci(arr.concat(1), len);
+    }
+
+    const arrLength = arr.length;
+    const value = arr[arrLength - 1] + arr[arrLength - 2];
+    return getFibonacci(arr.concat(value), len);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputContainer}>
+        Input:&nbsp;<input type='number' min={0} ref={inputRef} className={styles.inputField} />
+        <button className={styles.button} onClick={handleClick}>Submit</button>
+      </div>
+      {seq && (
+        <div>
+          Fibonacci Sequence:&nbsp;
+          {seq.map((num, i) => (<Pill key={i} variant='secondary'>{num}</Pill>))}
+        </div>
+      )}
+    </div>
+  );
+};
