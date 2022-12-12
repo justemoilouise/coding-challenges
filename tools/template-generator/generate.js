4fc3bbfd21b138f88e01ae32900c59b8b69bcb03
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-12-12 10:55:19 +0800

    Add challenge scaffolding script (#13)

---
+++ tools/template-generator/generate.js
@@ -0,0 +1,60 @@
+const { generateTemplateFiles } = require('generate-template-files');
+
+generateTemplateFiles([
+  {
+    option: '[Code Academy] Component',
+    defaultCase: '(kebabCase)',
+    entry: {
+        folderPath: './tools/template-generator/templates/code-academy/component-challenge-__challenge__.js',
+    },
+    stringReplacers: [{ question: 'Challenge number', slot: '__challenge__' }],
+    output: {
+        path: './src/code-academy/challenge-__challenge__.js',
+    },
+    onComplete: (results) => {
+        console.log(results);
+    },
+  },
+  {
+    option: '[Code Academy] Page',
+    defaultCase: '(kebabCase)',
+    entry: {
+        folderPath: './tools/template-generator/templates/code-academy/page-challenge-__challenge__.js',
+    },
+    stringReplacers: [{ question: 'Challenge number', slot: '__challenge__' }],
+    output: {
+        path: './src/pages/code-academy/challenge-__challenge__.js',
+    },
+    onComplete: (results) => {
+        console.log(results);
+    },
+  },
+  {
+    option: '[Edabit] Component',
+    defaultCase: '(kebabCase)',
+    entry: {
+        folderPath: './tools/template-generator/templates/edabit/component-challenge-__challenge__.js',
+    },
+    stringReplacers: [{ question: 'Challenge number', slot: '__challenge__' }],
+    output: {
+        path: './src/edabit/challenge-__challenge__.js',
+    },
+    onComplete: (results) => {
+        console.log(results);
+    },
+  },
+  {
+    option: '[Edabit] Page',
+    defaultCase: '(kebabCase)',
+    entry: {
+        folderPath: './tools/template-generator/templates/edabit/page-challenge-__challenge__.js',
+    },
+    stringReplacers: [{ question: 'Challenge number', slot: '__challenge__' }],
+    output: {
+        path: './src/pages/edabit/challenge-__challenge__.js',
+    },
+    onComplete: (results) => {
+        console.log(results);
+    },
+  },
+]);
---
+++ tools/template-generator/templates/code-academy/component-challenge-__challenge__.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as styles from '../styles/challenge.module.css';
+import classNames from 'classnames';
+
+export const Challenge = () => {
+  return (
+    <div className={classNames(styles.container, styles.gridContainer)}>
+  
+    </div>
+  );
+};
