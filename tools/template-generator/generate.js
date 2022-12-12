7852c343eaa3849fd7636eb676a416aa7fe20ae6
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-12-12 13:51:07 +1100

    edabit script

--- tools/template-generator/generate.js
+++ tools/template-generator/generate.js
@@ -29,4 +29,32 @@ generateTemplateFiles([
         console.log(results);
     },
   },
+  {
+    option: '[Edabit] Component',
+    defaultCase: '(kebabCase)',
+    entry: {
+        folderPath: './tools/template-generator/templates/edabit/component-challenge-__challenge__.js',
+    },
+    stringReplacers: [{ question: 'Challenge number', slot: '__challenge__' }],
+    output: {
+        path: './src/edabit/challenge-__challenge__.js',
+    },
+    onComplete: (results) => {
+        console.log(results);
+    },
+  },
+  {
+    option: '[Edabit] Page',
+    defaultCase: '(kebabCase)',
+    entry: {
+        folderPath: './tools/template-generator/templates/edabit/page-challenge-__challenge__.js',
+    },
+    stringReplacers: [{ question: 'Challenge number', slot: '__challenge__' }],
+    output: {
+        path: './src/pages/edabit/challenge-__challenge__.js',
+    },
+    onComplete: (results) => {
+        console.log(results);
+    },
+  },
 ]);
---
+++ tools/template-generator/templates/edabit/page-challenge-__challenge__.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Challenge } from '../../edabit/challenge-__challenge__';
+import { EdabitPageLayout } from '../../layouts/edabit-pageLayout';
+import data from '../../content/edabit.json';
+
+const breadcumbItems = [
+  {
+    link: '/edabit',
+    label: 'Edabit'
+  },
+  {
+    label: 'Challenge __challenge__',
+  },
+];
+
+const details = data[0];
+
+const Page = () => {
+  return (
+    <EdabitPageLayout
+      {...details}
+      title={`Challenge __challenge__ - ${details.title}`}
+      breadcumbItems={breadcumbItems}
+    >
+      <Challenge />
+    </EdabitPageLayout>
+  );
+};
+
+export default Page;
+
+export const Head = () => <title>Coding Challenges - Edabit - Challenge __challenge__</title>;
