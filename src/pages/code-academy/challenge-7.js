9cf900dfad4d99ebf8249f00251a4f1ac315ca7d
Louise Ann Apostol <louiseann.apostol@gmail.com>
2022-12-12 14:12:53 +1100

    code academy content + boilerplate

---
+++ src/pages/code-academy/challenge-7.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Challenge } from '../../code-academy/challenge-7';
+import { CodeAcademyPageLayout } from '../../layouts/codeAcademy-pageLayout';
+import data from '../../content/code-academy.json';
+
+const breadcumbItems = [
+  {
+    link: '/code-academy',
+    label: 'Code Academy'
+  },
+  {
+    label: 'Challenge 7',
+  },
+];
+
+const details = data[6];
+
+const Page = () => {
+  return (
+    <CodeAcademyPageLayout
+      {...details}
+      title={`Challenge 7 - ${details.title}`}
+      breadcumbItems={breadcumbItems}
+    >
+      <Challenge />
+    </CodeAcademyPageLayout>
+  );
+};
+
+export default Page;
+
+export const Head = () => <title>Coding Challenges - Code Academy - Challenge 7</title>;
