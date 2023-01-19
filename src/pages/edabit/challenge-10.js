194e1ce826d54ba28fd936faa86abfb4d2757c47
Louise Ann Apostol <louiseann.apostol@gmail.com>
2023-01-20 09:34:30 +1100

    edabit content + boilerplate

---
+++ src/pages/edabit/challenge-10.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Challenge } from '../../edabit/challenge-10';
+import { EdabitPageLayout } from '../../layouts/edabit-pageLayout';
+import data from '../../content/edabit.json';
+
+const breadcumbItems = [
+  {
+    link: '/edabit',
+    label: 'Edabit'
+  },
+  {
+    label: 'Challenge 10',
+  },
+];
+
+const details = data[9];
+
+const Page = () => {
+  return (
+    <EdabitPageLayout
+      {...details}
+      title={`Challenge 10 - ${details.title}`}
+      breadcumbItems={breadcumbItems}
+    >
+      <Challenge />
+    </EdabitPageLayout>
+  );
+};
+
+export default Page;
+
+export const Head = () => <title>Coding Challenges - Edabit - Challenge 10</title>;
