0fd0de56261849c7bf5b0ceba5087f3ba9f2c7ed
Louise Ann Apostol <louiseann.apostol@gmail.com>
2023-01-31 11:17:35 +1100

    code academy content + boilerplate

---
+++ src/pages/code-academy/challenge-12.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Challenge } from '../../code-academy/challenge-12';
+import { CodeAcademyPageLayout } from '../../layouts/codeAcademy-pageLayout';
+import data from '../../content/code-academy.json';
+
+const breadcumbItems = [
+  {
+    link: '/code-academy',
+    label: 'Code Academy'
+  },
+  {
+    label: 'Challenge 12',
+  },
+];
+
+const details = data[11];
+
+const Page = () => {
+  return (
+    <CodeAcademyPageLayout
+      {...details}
+      title={`Challenge 12 - ${details.title}`}
+      breadcumbItems={breadcumbItems}
+    >
+      <Challenge />
+    </CodeAcademyPageLayout>
+  );
+};
+
+export default Page;
+
+export const Head = () => <title>Coding Challenges - Code Academy - Challenge 12</title>;
